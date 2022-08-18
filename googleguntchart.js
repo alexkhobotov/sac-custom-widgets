05decfd2dae018b623647b619e921ccaafbb6c48
alexkhobotov <alex.khobotov@gmail.com>
2022-08-19 00:20:21 +0300

    dataset usage

--- googleguntchart.js
+++ googleguntchart.js
@@ -100,8 +100,9 @@
 				const dataBinding = this.dataBindings.getDataBinding('myDataBinding');
 				let data_set = await dataBinding.getDataSource().getResultSet();
 				console.log(data_set);
-				let widget_data = await this.getData(); //how to make shure that data is loaded?
-				let prepared_data = await this.prepareData(widget_data);
+				//let widget_data = await this.getData(); //how to make shure that data is loaded?
+				//let prepared_data = await this.prepareData(widget_data);
+				let prepared_data = await this.dataSetToGoogleData(data_set);
 				console.log(prepared_data);
 				data = prepared_data;
 				GoogleChart();
@@ -128,6 +129,28 @@
 					prepared_data.addRows([
 						[row.dimensions_0.id, row.dimensions_1.id, row.dimensions_2.id,
 						new Date(row.dimensions_3.id), new Date(row.dimensions_4.id), null,  100,  (row.dimensions_6.id === '#') ? null : row.dimensions_6.id]
+						]);
+				})
+			}
+			return prepared_data;
+		}
+
+		async dataSetToGoogleData(data_set){
+			let prepared_data = new google.visualization.DataTable();
+			prepared_data.addColumn('string', 'Task ID');
+			prepared_data.addColumn('string', 'Task Name');
+			prepared_data.addColumn('string', 'Resource');
+			prepared_data.addColumn('date', 'Start Date');
+			prepared_data.addColumn('date', 'End Date');
+			prepared_data.addColumn('number', 'Duration');
+			prepared_data.addColumn('number', 'Percent Complete');
+			prepared_data.addColumn('string', 'Dependencies');
+			if (data_set){
+				data_set.forEach(row => {
+					console.log(row);
+					prepared_data.addRows([
+						[row.Event_Id.id, row.Event_Name.id, row.Event_Category.id,
+						new Date(row.Start_Date.id), new Date(row.End_Date.id), null,  100,  (row.Dependent_On.id === '#') ? null : row.Dependent_On.id]
 						]);
 				})
 			}
