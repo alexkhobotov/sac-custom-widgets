a35c00ad6686be2fb53ecbb79841964d8ca66736
alexkhobotov <alex.khobotov@gmail.com>
2022-08-19 12:54:16 +0300

    .

--- googleguntchart.js
+++ googleguntchart.js
@@ -47,6 +47,28 @@
 		google.charts.setOnLoadCallback(function(){ drawChart(prepared_data) });
     }
 
+	function dataSetToGoogleData(data_set){
+		let prepared_data = new google.visualization.DataTable();
+		prepared_data.addColumn('string', 'Task ID');
+		prepared_data.addColumn('string', 'Task Name');
+		prepared_data.addColumn('string', 'Resource');
+		prepared_data.addColumn('date', 'Start Date');
+		prepared_data.addColumn('date', 'End Date');
+		prepared_data.addColumn('number', 'Duration');
+		prepared_data.addColumn('number', 'Percent Complete');
+		prepared_data.addColumn('string', 'Dependencies');
+		if (data_set){
+			data_set.forEach(row => {
+				console.log(row);
+				prepared_data.addRows([
+					[row.Event_Id.id, row.Event_Name.id, row.Event_Category.id,
+					new Date(row.Start_Date.id), new Date(row.End_Date.id), null,  100,  (row.Dependent_On.id === '#') ? null : row.Dependent_On.id]
+					]);
+			})
+		}
+		return prepared_data;
+	}
+
 	class GoogleGunttChart extends HTMLElement {
 		constructor() {
 			super(); 
@@ -97,32 +119,10 @@
 				const dataBinding = that.dataBindings.getDataBinding('myDataBinding');
 				return dataBinding.getDataSource().getResultSet();
 			}).then((data_set)=>{
-				return that.dataSetToGoogleData(data_set);
+				return dataSetToGoogleData(data_set);
 			}).then((prepared_data)=>{
 				GoogleChart(prepared_data);
 			});
-		}
-
-		dataSetToGoogleData(data_set){
-			let prepared_data = new google.visualization.DataTable();
-			prepared_data.addColumn('string', 'Task ID');
-			prepared_data.addColumn('string', 'Task Name');
-			prepared_data.addColumn('string', 'Resource');
-			prepared_data.addColumn('date', 'Start Date');
-			prepared_data.addColumn('date', 'End Date');
-			prepared_data.addColumn('number', 'Duration');
-			prepared_data.addColumn('number', 'Percent Complete');
-			prepared_data.addColumn('string', 'Dependencies');
-			if (data_set){
-				data_set.forEach(row => {
-					console.log(row);
-					prepared_data.addRows([
-						[row.Event_Id.id, row.Event_Name.id, row.Event_Category.id,
-						new Date(row.Start_Date.id), new Date(row.End_Date.id), null,  100,  (row.Dependent_On.id === '#') ? null : row.Dependent_On.id]
-						]);
-				})
-			}
-			return prepared_data;
 		}
 	}
 
